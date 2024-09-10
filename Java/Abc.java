b229781d8f5b5b4f9725a48ebd6019535d11f981
dhakadrohan98 <rohandhakad20@gmail.com>
2024-09-10 20:00:16 +0530

    custom, myubuntu & myjavaprogram images are build using docker cli & containers are created from this custom image

---
+++ Java/Abc.java
@@ -0,0 +1,8 @@
+public class Abc {
+
+	public static void main(String[] args) {
+		System.out.println("*****MY java application******");
+		System.out.println("This is compiled & executed by Docker");
+		System.out.println("*****THANK YOU******");
+	}
+}
